83c9039a859ebd3781062945005a626ea43dfb6c
june <juner830310@gmamil.com>
2019-05-31 17:28:33 +0900

    build start script

---
+++ build.bat
@@ -0,0 +1,3 @@
+meteor build --directory C:/Users/Furo/wisecare/projects/build --architecture os.windows.x86_32
+cd C:/Users/Furo/wisecare/projects/build
+npm install
---
+++ start.bat
@@ -0,0 +1,4 @@
+SET MONGO_URL=mongodb://wisecare:wisecare123@localhost:27017/wisecare?replicaSet=wisecare
+SET ROOT_URL=http://localhost:12000
+SET PORT=12000
+node main.js
