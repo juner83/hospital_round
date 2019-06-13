07925830d0abebe11f773f57bd5754ae385de5ab
june <juner830310@gmail.com>
2019-06-13 14:20:57 +0900

    build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,15 @@
-meteor build --directory C:/Users/Furo/wisecare/projects/build --architecture os.windows.x86_32
-cd C:/Users/Furo/wisecare/projects/build
-npm install
+@echo off
+
+set projectdir=C:\Users\Furo\wisecare\projects
+set builddir=%projectdir%\build
+set timestring=%date:~-10,4%%date:~-5,2%%date:~8,2%
+set timestring=%timestring%%time:~0,2%%time:~3,2%%time:~6,2%
+
+copy %builddir%\bundle\start.bat %builddir%
+ren %builddir%\bundle bundle%timestring%
+
+meteor build --directory %builddir% --architecture os.windows.x86_32
+
+bandizip x -y -o:%builddir%\bundle\programs\server\node_modules %builddir%\node_modules.zip 
+
+copy %builddir%\start.bat  %builddir%\bundle\
